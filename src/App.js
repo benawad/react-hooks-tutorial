82004bc591ffa6b5998ae5b07d4a7ab50e20177d
Ben Awad <benawad97@gmail.com>
2019-06-21 19:26:25 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useForm } from "./useForm";
 
-function App() {
+const App = () => {
+  const [values, handleChange] = useForm({ email: "", password: "" });
+  const [values2, handleChange2] = useForm({ firstName: "", lastName: "" });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <>
+        <input name="email" value={values.email} onChange={handleChange} />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </>
     </div>
   );
-}
+};
 
 export default App;
