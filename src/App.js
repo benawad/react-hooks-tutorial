2d6a84e2673ccf9c658b8305cdaaedd0c572fb03
Ben Awad <benawad97@gmail.com>
2019-06-25 18:41:55 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,54 +1,10 @@
-import React, { useState, useRef, useLayoutEffect } from "react";
-import { useForm } from "./useForm";
+import React from "react";
 import { Hello } from "./Hello";
-import { useMeasure } from "./useMeasure";
 
 const App = () => {
-  const [values, handleChange] = useForm({
-    email: "",
-    password: "",
-    firstName: ""
-  });
-  const inputRef = useRef();
-  const hello = useRef(() => console.log("hello"));
-
-  const [showHello, setShowHello] = useState(true);
-
-  const [rect, inputRef2] = useMeasure([]);
-
   return (
     <div>
-      <>
-        <button onClick={() => setShowHello(!showHello)}>toggle</button>
-        {showHello && <Hello />}
-        <input
-          ref={inputRef}
-          name="email"
-          value={values.email}
-          onChange={handleChange}
-        />
-        <input
-          ref={inputRef2}
-          name="firstName"
-          placeholder="first name"
-          value={values.firstName}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-        />
-        <button
-          onClick={() => {
-            inputRef.current.focus();
-            hello.current();
-          }}
-        >
-          focus
-        </button>
-      </>
+      <Hello />
     </div>
   );
 };
--- src/Hello.js
+++ src/Hello.js
@@ -1,27 +1,5 @@
-import React, { useRef, useState, useEffect, useLayoutEffect } from "react";
-import { useFetch } from "./useFetch";
-import { useMeasure } from "./useMeasure";
+import React from "react";
 
 export const Hello = () => {
-  // const renders = useRef(0);
-  const [count, setCount] = useState(() =>
-    JSON.parse(localStorage.getItem("count"))
-  );
-  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
-  useEffect(() => {
-    localStorage.setItem("count", JSON.stringify(count));
-  }, [count]);
-
-  const [rect, divRef] = useMeasure([data]);
-
-  return (
-    <div>
-      <div style={{ display: "flex" }}>
-        <div ref={divRef}>{!data ? "loading..." : data}</div>
-      </div>
-      <pre>{JSON.stringify(rect, null, 2)}</pre>
-      <div>count: {count}</div>
-      <button onClick={() => setCount(c => c + 1)}>increment</button>
-    </div>
-  );
+  return <div>hello</div>;
 };
