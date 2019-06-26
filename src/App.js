4a5200a39a9b06a1df9ee6afd68153a47ed847af
Ben Awad <benawad97@gmail.com>
2019-06-26 18:03:24 -0500

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React, { useState, useCallback } from "react";
-import { Hello } from "./Hello";
-import { Square } from "./Square";
+import React, { useState, useMemo } from "react";
+import { useFetch } from "./useFetch";
+
+function computeLongestWord(arr) {
+  if (!arr) {
+    return [];
+  }
+
+  console.log("computing longest word");
+
+  let longestWord = "";
+
+  JSON.parse(arr).forEach(sentence =>
+    sentence.split(" ").forEach(word => {
+      if (word.length > longestWord.length) {
+        longestWord = word;
+      }
+    })
+  );
+
+  return longestWord;
+}
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const favoriteNums = [7, 21, 37];
-
-  const increment = useCallback(
-    n => {
-      setCount(c => c + n);
-    },
-    [setCount]
+  const { data } = useFetch(
+    "https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json"
   );
+
+  const longestWord = useMemo(() => computeLongestWord(data), [data]);
 
   return (
     <div>
-      <Hello increment={increment} />
       <div>count: {count}</div>
-      {favoriteNums.map(n => {
-        return <Square increment={increment} n={n} key={n} />;
-      })}
+      <button onClick={() => setCount(count + 1)}>increment</button>
+      <div>{longestWord}</div>
     </div>
   );
 };
